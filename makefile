02d4e2f2c90bb0161822d5afaf1351b2f161bb91
axhar005 <moi@moi.moi>
2024-01-15 21:07:40 +0000

    wip

--- makefile
+++ makefile
@@ -1,18 +1,26 @@
 #files variable
 DOCKERCONFDIR="$(HOME)/.config/appdata"
 
-all: dependencies env
+all:
+	@cat .header
 
 dependencies:
 	@sudo apt update
 	@sudo chmod +x psk.sh
+	@sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+	@curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	@sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $$(lsb_release -cs) stable"
+	@sudo apt-get update
+	@sudo apt-get install docker-ce
 	@sudo curl -L "https://github.com/docker/compose/releases/download/v2.24.0/docker-compose-$(shell uname -s)-$(shell uname -m)" -o /usr/local/bin/docker-compose
 	@sudo chmod +x /usr/local/bin/docker-compose
 	@docker-compose --version
+	@sudo usermod -aG docker $(USER)
+	@newgrp docker
 
 setup:
-	mkdir -p $(DOCKERCONFDIR)
-	mkdir -p $(DOCKERCONFDIR)/psk
+	@mkdir -p $(DOCKERCONFDIR)
+	@mkdir -p $(DOCKERCONFDIR)/psk
 	@sudo cp -r . $(DOCKERCONFDIR)/psk
 	@sudo cp psk.sh /usr/local/bin/psk
 
--- psk.sh
+++ psk.sh
@@ -3,27 +3,29 @@
 
 #files variable
 DOCKERCONFDIR="$HOME/.config/appdata"
-PSKDIR=$DOCKERCONFDIR/psk
-DOCKERSTORAGEDIR=$(eval echo $(grep 'DOCKERSTORAGEDIR=' $PSKDIR/.env | cut -d '=' -f2))
-VPN_USER=$(eval echo $(grep 'VPN_USER=' $PSKDIR/.env | cut -d '=' -f2))
-VPN_PASS=$(eval echo $(grep 'VPN_PASS=' $PSKDIR/.env | cut -d '=' -f2))
-PLEX_CLAIM=$(eval echo $(grep 'PLEX_CLAIM=' $PSKDIR/.env | cut -d '=' -f2))
-PLEX_IP=$(eval echo $(grep 'PLEX_IP=' $PSKDIR/.env | cut -d '=' -f2))
+PSK_DIR=$DOCKERCONFDIR/psk
+SONARR_DIR=$DOCKERCONFDIR/sonarr
+RADARR_DIR=$DOCKERCONFDIR/radarr
+PROWLARR_DIR=$DOCKERCONFDIR/prowlarr
+OVERSEERR_DIR=$DOCKERCONFDIR/overseerr
+DOCKERSTORAGEDIR=$(eval echo $(grep 'DOCKERSTORAGEDIR=' $PSK_DIR/.env | cut -d '=' -f2))
+VPN_USER=$(eval echo $(grep 'VPN_USER=' $PSK_DIR/.env | cut -d '=' -f2))
+VPN_PASS=$(eval echo $(grep 'VPN_PASS=' $PSK_DIR/.env | cut -d '=' -f2))
+PLEX_CLAIM=$(eval echo $(grep 'PLEX_CLAIM=' $PSK_DIR/.env | cut -d '=' -f2))
+PLEX_IP=$(eval echo $(grep 'PLEX_IP=' $PSK_DIR/.env | cut -d '=' -f2))
 
 #main
 if [ "$1" = "-help" ] || [ "$1" = "help" ]; then
-	echo "help $PSKDIR"
-	echo ${DOCKERSTORAGEDIR}
-	echo $VPN_USER
+	cat $PSK_DIR/.help
 elif [ "$1" = "start" ]; then
 	echo "plexStarterKit start"
-	docker compose -f $PSKDIR up -d
+	docker compose -f $PSK_DIR up -d
 elif [ "$1" = "stop" ]; then
 	echo "plexStarterKit stop"
-	docker compose -f $PSKDIR down
+	docker compose -f $PSK_DIR down
 elif [ "$1" = "restart" ]; then
 	echo "plexStarterKit restart"
-	docker compose -f $PSKDIR down && docker compose -f $PSKDIR up -d
+	docker compose -f $PSK_DIR down && docker compose -f $PSK_DIR up -d
 elif [ "$1" = "env" ]; then
 	# read -e -p "Docker configuration directory (default: $HOME/.config/appdata): " DOCKERCONFDIR
 	# DOCKERCONFDIR="$HOME/.config/appdata"
@@ -31,33 +33,33 @@ elif [ "$1" = "env" ]; then
 	read -e -p "Storage directory (default: /data): " DOCKERSTORAGEDIR
 	DOCKERSTORAGEDIR=${DOCKERSTORAGEDIR:-/data}
 
-	read -p "Username for VPN (default: your_vpn_username): " VPN_USER
+	read -e -p "Username for VPN (default: your_vpn_username): " VPN_USER
 	VPN_USER=${VPN_USER:-your_vpn_username}
 
-	read -p "Password for VPN (default: your_vpn_password): " VPN_PASS
+	read -e -p "Password for VPN (default: your_vpn_password): " VPN_PASS
 	VPN_PASS=${VPN_PASS:-your_vpn_password}
 
-	read -p "Plex claim (default: your_claim'https://www.plex.tv/claim/'): " PLEX_CLAIM
+	read -e -p "Plex claim (default: your_claim'https://www.plex.tv/claim/'): " PLEX_CLAIM
 	PLEX_CLAIM=${PLEX_CLAIM:-your_claim}
 
-	read -p "PLEX IP (default: http://192.168.0.120:32400/): " PLEX_IP
+	read -e -p "PLEX IP (default: http://192.168.0.120:32400/): " PLEX_IP
 	PLEX_IP=${PLEX_IP:-http://192.168.0.120:32400/}
 
-	sed -i 's|DOCKERSTORAGEDIR=.*|DOCKERSTORAGEDIR='"'$DOCKERSTORAGEDIR'"'|' .env
-	sed -i 's|VPN_USER=.*|VPN_USER='"'$VPN_USER'"'|' .env
-	sed -i 's|VPN_PASS=.*|VPN_PASS='"'$VPN_PASS'"'|' .env
-	sed -i 's|DOCKERCONFDIR=.*|DOCKERCONFDIR='"'$DOCKERCONFDIR'"'|' .env
-	sed -i 's|PLEX_CLAIM=.*|PLEX_CLAIM='"'$PLEX_CLAIM'"'|' .env
-	sed -i 's|PLEX_IP=.*|PLEX_IP='"'$PLEX_IP'"'|' .env
+	sed -i 's|DOCKERSTORAGEDIR=.*|DOCKERSTORAGEDIR='"'$DOCKERSTORAGEDIR'"'|' $PSK_DIR/.env
+	sed -i 's|VPN_USER=.*|VPN_USER='"'$VPN_USER'"'|' $PSK_DIR/.env
+	sed -i 's|VPN_PASS=.*|VPN_PASS='"'$VPN_PASS'"'|' $PSK_DIR/.env
+	sed -i 's|DOCKERCONFDIR=.*|DOCKERCONFDIR='"'$DOCKERCONFDIR'"'|' $PSK_DIR/.env
+	sed -i 's|PLEX_CLAIM=.*|PLEX_CLAIM='"'$PLEX_CLAIM'"'|' $PSK_DIR/.env
+	sed -i 's|PLEX_IP=.*|PLEX_IP='"'$PLEX_IP'"'|' $PSK_DIR/.env
 
-elif [ "$1" = "files" ]; then
+elif [ "$1" = "init-storage" ]; then
 	if [ -z "$DOCKERCONFDIR" ] || [ -z "$DOCKERSTORAGEDIR" ] || [ -z "$VPN_USER" ] || [ -z "$VPN_PASS" ] || [ -z "$PLEX_CLAIM" ] || [ -z "$PLEX_IP" ]; then
 		echo "env variables missing"
 		exit 1
 	fi
 
 	if [ ! -z "$DOCKERSTORAGEDIR" ]; then
-		mkdir -p "${DOCKERSTORAGEDIR}"
+		sudo mkdir -p "$DOCKERSTORAGEDIR"
 		sudo chown -R $USER:$USER "${DOCKERSTORAGEDIR}"
 		sudo chmod -R a=,a+rX,u+w,g+w "${DOCKERSTORAGEDIR}"
 		mkdir -p "$DOCKERSTORAGEDIR/downloads"
@@ -68,7 +70,7 @@ elif [ "$1" = "files" ]; then
 		mkdir -p "$DOCKERSTORAGEDIR/media/movies"
 		mkdir -p "$DOCKERSTORAGEDIR/media/series"
 	fi
-elif [ "$1" = "configfiles" ]; then
+elif [ "$1" = "init-config" ]; then
 	if [ ! -z "$DOCKERCONFDIR" ]; then
 		mkdir -p "$DOCKERCONFDIR"
 		mkdir -p "$DOCKERCONFDIR/psk"
@@ -79,10 +81,13 @@ elif [ "$1" = "configfiles" ]; then
 		mkdir -p "$DOCKERCONFDIR/plex"
 		mkdir -p "$DOCKERCONFDIR/overseerr"
 	fi
+
+elif [ "$1" = "status" ]; then
+	docker ps
+
 elif [ "$1" = "unistall" ]; then
 	sudo rm -rf '/usr/local/bin/psk'
-	sudo rm -rf $PSKDIR
-	# sudo rm -rf $DOCKERSTORAGEDIR/*
+	sudo rm -rf $PSK_DIR
 else
 	echo "unknow commande $1"
 fi
