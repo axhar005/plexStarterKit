7b6e2353433376d6306d73e065658e4639675654
axhar005 <olivier.boucher.game@hotmail.com>
2024-05-23 13:05:59 -0400

    update psk.sh

--- psk.sh
+++ psk.sh
@@ -13,29 +13,32 @@ PLEX_IP=$(eval echo $(grep 'PLEX_IP=' $PSK_DIR/.env | cut -d '=' -f2))
 #main
 # HELP
 if [ "$1" = "help" ] || [ "$1" = "-help" ] || [ "$1" = "-h" ]; then
+	echo "> psk execute $1"
 	cat $PSK_DIR/.help
 
 # START
 elif [ "$1" = "run" ] || [ "$1" = "up" ] || [ "$1" = "-r" ]; then
-	echo "psk: start"
+	echo "> psk execute $1"
 	docker compose -f $PSK_DIR/docker-compose.yml up -d
 
 # STOP
 elif [ "$1" = "stop" ] || [ "$1" = "down" ] || [ "$1" = "-s" ]; then
-	echo "psk: stop"
+	echo "> psk execute $1"
 	docker compose -f $PSK_DIR/docker-compose.yml down
 
 # RESTART
 elif [ "$1" = "restart" ]; then
-	echo "psk: restart"
+	echo "> psk execute $1"
 	docker compose -f $PSK_DIR/docker-compose.yml down && docker compose -f $PSK_DIR/docker-compose.yml up -d
 
 # ENV
 elif [ "$1" = "env" ]; then
+	echo "> psk execute env"
 	bash $COMMAND_DIR/env.sh setup
 
 # STORAGE
 elif [ "$1" = "storage" ]; then
+	echo "> psk execute storage"
 	bash "$COMMAND_DIR/storage.sh"
 
 # VPN
@@ -44,6 +47,7 @@ elif [ "$1" = "vpn" ]; then
 
 # SHOW
 elif [ "$1" = "show" ] || [ "$1" = "-s" ]; then
+	echo "> psk execute $1"
 	if [ "$2" = "" ]; then
 		cat $PSK_DIR/.help
 	else
@@ -52,21 +56,25 @@ elif [ "$1" = "show" ] || [ "$1" = "-s" ]; then
 
 # EDIT
 elif [ "$1" = "edit" ] || [ "$1" = "-e" ]; then
+	echo "> psk execute $1"
 	if [ "$2" = "env" ]; then
 		nano -m $PSK_DIR/.env
 	fi
 
 # STATUS
 elif [ "$1" = "status" ] || [ "$1" = "ps" ]; then
+	echo "> psk execute $1"
 	docker ps
 
 # UPDATE
 elif [ "$1" = "update" ]; then
+	echo "> psk execute $1"
 	(cd "$PSK_DIR" && git pull)
 	bash $COMMAND_DIR/update_psk.sh
 
 #UNISTALL
 elif [ "$1" = "unistall" ]; then
+	echo "> psk execute $1"
 	bash $COMMAND_DIR/unistall.sh
 
 else
